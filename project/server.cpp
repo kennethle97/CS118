49b7c3574a6b91902fe9239e153d5efb77bbba3a
root <root@LAPTOP-L1LKO0Q6>
2023-04-28 15:02:33 -0700

    Implemented server request handling txt and jpg files

--- project/server.cpp
+++ project/server.cpp
@@ -8,13 +8,33 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <netinet/in.h>
 
 
 #define MY_PORT 8080
-#define BACKLOG 100 /*Pending connections queue size*/
+#define BACKLOG 10 /*Pending connections queue size*/
 #define BUFFER_SIZE 1024
+
+
+
+const char *mime_type(char* filename){
+    const char *mime_type = "application/octet-stream";
+    if (strstr(filename,".txt")){
+        mime_type = "text/plain";
+    }
+    else if(strstr(filename, ".html") || strstr(filename, ".htm")){
+        mime_type = "text/html";
+    }
+    else if (strstr(filename,".jpg") || strstr (filename,"jpeg")){
+        mime_type = "image/jpg";
+    }
+    else if (strstr(filename,".png")){
+        mime_type = "image/png";
+    }
+    return mime_type;
+}
 
 
 int main(int argc, char*argv[])
@@ -45,6 +65,7 @@ int main(int argc, char*argv[])
             exit(1);
         }
 
+    /*Fixes error when port number is in use.*/
     int yes=1;
 
     if(setsockopt(sock_fd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof yes) == -1){
@@ -59,20 +80,60 @@ int main(int argc, char*argv[])
 
      printf("Listening to socket %d\n",port_number);
 
+     printf("Waiting to accept connection from client.\n");
+
     while(1){
-
-        printf("Waiting to accept connection from client.\n");
-
+        //Accepting client connections to server 
         client_fd = accept(sock_fd, (struct sockaddr *) &client_addr,&client_addr_size); 
         if(client_fd == -1){
             perror("Error in accepting client connection");
             exit(1);
         }
-
+        //getting client_ip address and port number and printing it to console.
         char client_ip[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &client_addr.sin_addr, client_ip,sizeof(client_ip));
         int client_port = ntohs(client_addr.sin_port);
         printf("Accepted connection from client:\n %s:%d\n", client_ip, client_port);
 
-    }
+
+        //Now to read HTTP request from client
+
+        memset(buffer, 0, BUFFER_SIZE);
+        recv(client_fd, buffer, BUFFER_SIZE, 0);
+        printf("Request Received:\n%s\n",buffer);
+
+        //Parse HTTP request for filename
+        char filename[256] = {0};
+        sscanf(buffer, "GET /%s HTTP/1.1",filename);
+
+
+        //Now to open file and read read contents into buffer
+        struct stat file_stat;
+        char* file_content;
+        int file_size;
+
+        int fd = open(filename,O_RDONLY);
+        if (fd == -1){
+            char response[] = "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n";
+            send(client_fd,response,strlen(response),0);
+            }
+        else{
+            fstat(fd, &file_stat);
+            file_size = file_stat.st_size;
+            file_content = (char*)malloc(file_size+1);
+            memset(file_content,0,file_size);
+            read(fd, file_content, file_size);
+            const char* mime_typestr = mime_type(filename);
+            char response[BUFFER_SIZE] = {0};
+            sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContent-Length: %ld\r\n\r\n", mime_typestr, file_size);
+            send(client_fd, response, strlen(response), 0);
+            send(client_fd, file_content, file_size, 0);
+            
+            free(file_content);
+        }
+
+        close(fd);
+        }
+    close(client_fd);
+    // free(file_content)
 }
